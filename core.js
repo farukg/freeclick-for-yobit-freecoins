9c2ed31d0753178e54a3164151b9a6d0025d6ed0
Mustafa Yalçın <iletisim@sahvemat.com>
2016-04-14 19:07:16 +0300

    check fix

--- core.js
+++ core.js
@@ -2,25 +2,33 @@ var c_=0;
 var ca_=500;
 var s_=false;
 var oC;
-$("tr td .countdown.green").parent().parent().find('.clGetFreeCoins').each(function(this){
+
+$("tr td .countdown.green").parent().parent().find('.clGetFreeCoins').each(function(i){
 	s_=true;
 	c_=c_+ca_;
 	oC=setTimeout(sf_(this),c_);
 });
 function sf_(o){
-	var c=$("#window_captcha");
-	if(c.css('display')=='none'){
-		o.click();
-	}else{
-		s_=false;
-	}
-	var b=$(".messi-actions .btnbox button");
-	if(b.css('display')!='none'){
-		b.click();
+	var r_=cf_();
+	if(r_){
+		var c=$("#window_captcha");
+		if(c.css('display')=='none'){
+			o.click();
+		}else{
+			s_=false;
+		}
+		var b=$(".messi-actions .btnbox button");
+		if(b.css('display')!='none'){
+			b.click();
+		}
 	}
 }
 function cf_(){
+	var c=$("#window_captcha");
 	if(c.css('display')=='block'){
 		clearTimeout(oC);
+		return false;
+	}else{
+		return true;
 	}
 }
